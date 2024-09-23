cada00773ce86b5756efe8d11b92848e4bf3fd85
Björn Hegstam <bjorn.hegstam@gmail.com>
2024-09-23 12:35:13 +0200

    Convert Sprite to record

--- XNALibrary/Sprites/Sprite.cs
+++ XNALibrary/Sprites/Sprite.cs
@@ -3,8 +3,4 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace XNALibrary.Sprites;
 
-public class Sprite(Texture2D texture, Rectangle sourceRect)
-{
-    public Texture2D Texture { get;  } = texture;
-    public Rectangle SourceRectangle { get; } = sourceRect;
-}
+public record Sprite(Texture2D Texture, Rectangle SourceRectangle);
